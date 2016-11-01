0752d8f526bffe221c35ce4003815e3da622a706
Daniel Davidsen <danieldavidsen@gmail.com>
2016-11-01 16:04:19 +0100

    I sometimes suck at commiting

---
+++ sortNames.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(void) {
+
+    int value1, value2, sum;
+
+    value1 = 50;
+    value2 = 25;
+    sum = value1 + value2;
+
+    printf("Her kan du jo se hvor mye %i og %i blir, nemmelig %i", value1, value2, sum);
+    return 0;
+}
